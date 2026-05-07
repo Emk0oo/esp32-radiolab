ac94e3347dabf5dbc41c1443bb9743298a1a7811
Javor Emir <emir.j2002@gmail.com>
2026-05-08 00:39:17 +0200

    Add ESP32-S3 diagnostic functions and update platformio configuration

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,30 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+void esp32s3Specs(){
+    Serial.println("=== ESP32-S3 Diagnostic ===");
+  Serial.printf("Chip : %s\n", ESP.getChipModel());
+  Serial.printf("Revision : %d\n", ESP.getChipRevision());
+  Serial.printf("Cores : %d\n", ESP.getChipCores());
+  Serial.printf("CPU Freq : %d MHz\n", ESP.getCpuFreqMHz());
+  Serial.printf("Flash : %d MB\n", ESP.getFlashChipSize() / (1024 * 1024));
+  Serial.printf("PSRAM : %d MB\n", ESP.getPsramSize() / (1024 * 1024));
+  Serial.printf("Heap : %d KB\n", ESP.getFreeHeap() / 1024);
+}
+
+void esp32S3Boot() {
+  Serial.println("ESP32-S3 booting...");
+  esp32s3Specs();
+}
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  delay(1000);
+  esp32S3Boot();
+  Serial.println("=== Setup done ===");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  delay(5000);
+  Serial.println("alive");
 }
